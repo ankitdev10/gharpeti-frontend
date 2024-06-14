7e81989bd858c253abb9f050b7b7cb34589287c6
ankitdev10 <leoankit35@gmail.com>
2024-06-14 21:45:44 +0545

    feat: setup tanstack query

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -4,7 +4,7 @@ const AuthLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
       <main className="">
-        <div className="grid grid-cols-1 md:grid-cols-2 h-screen w-full">
+        <div className="grid grid-cols-1 md:grid-cols-2 divide-x h-screen w-full">
           <div className="flex flex-col justify-center items-center p-8 md:p-12 lg:p-16">
             <div className="max-w-md space-y-4">
               <h1 className="text-3xl md:text-4xl lg:text-5xl font-bold">
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Inter, Poppins } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "next-themes";
+import Providers from "@/components/providers";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -27,14 +29,17 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${inter.variable} ${poppins.variable} font-poppins`}>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="light"
-          enableSystem
-          disableTransitionOnChange
-        >
-          {children}
-        </ThemeProvider>
+        <Providers>
+          <ReactQueryDevtools initialIsOpen={false} />
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="light"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+          </ThemeProvider>
+        </Providers>
       </body>
     </html>
   );
